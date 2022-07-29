c8febe55c48a626add33bfcf420c9e02c5712729
JAE <jack3787@naver.com>
2022-07-29 21:03:44 +0900

    New : 입출력과 사칙연산 - Hello World 출력 (내 답안)

---
+++ src/InputOutputAndCalculations_1st/Q2557.java
@@ -0,0 +1,13 @@
+package InputOutputAndCalculations_1st;
+
+/**
+ *  문제:
+ *  Hello World를 출력하시오.
+ */
+
+public class Q2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
