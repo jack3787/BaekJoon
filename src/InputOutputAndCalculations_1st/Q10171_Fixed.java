23393a1e547538907cf8f020e1c3c35b59375c26
JAE <jack3787@naver.com>
2022-08-07 20:44:54 +0900

    Fixed : 입출력 - 고양이

---
+++ src/InputOutputAndCalculations_1st/Q10171_Fixed.java
@@ -0,0 +1,27 @@
+/**
+ *  주제: 고양이
+ *
+ *  문제:
+ *  아래 예제와 같이 고양이를 출력하시오.
+ *
+ *  예제:
+ *  \    /\
+ *  )  ( ')
+ *  (  /  )
+ *  \(__)|
+ *
+ *  힌트:
+ *
+ */
+
+package InputOutputAndCalculations_1st;
+
+public class Q10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\ ");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
