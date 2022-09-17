325a0726d77ea4cc51fc458be0a2a0a749aabe90
JAE <jack3787@naver.com>
2022-09-18 01:15:15 +0900

    correct : Q2739 - 구구단

---
+++ src/plzrun_1_InputOutput/Q2739_Correct.java
@@ -0,0 +1,21 @@
+/**
+ * 주제: 구구단
+ */
+
+package plzrun_1_InputOutput;
+
+import java.util.Scanner;
+
+public class Q2739_Correct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+
+
+    }
+}
