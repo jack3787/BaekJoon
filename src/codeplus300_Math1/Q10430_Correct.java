988ea8be7471b27c9db395eaecd4be36f90f83ee
JAE <jack3787@naver.com>
2022-10-23 19:33:29 +0900

    Correct : Q10430 - 나머지

---
+++ src/codeplus300_Math1/Q10430_Correct.java
@@ -0,0 +1,23 @@
+/**
+ * 주제: 나머지
+ */
+
+package codeplus300_Math1;
+
+import java.util.Scanner;
+
+public class Q10430_Correct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+
+    }
+}
